a1a3604a2b1ed67c7d76072a7c6ef239c0eb3ba6
Andreas Kahler <mail@andreaskahler.com>
2023-12-29 18:17:28 +0100

    prepare 0.2.1 release

--- setup.py
+++ setup.py
@@ -2,11 +2,21 @@ from setuptools import setup, find_packages
 
 setup(
     name='cadscript',
-    version='0.2',
+    description = "A Python module for creating 3D models with scripts.",
+    long_description="A Python module for creating 3D models with scripts.",
+    version='0.2.1',
+    url="https://github.com/CadQuery/cadquery",
+    license="Apache Public License 2.0",    
     author='Andreas Kahler',
     author_email='mail@andreaskahler.com',
     packages=find_packages(),
     install_requires=[
         'cadquery'
     ],
+    python_requires=">=3.8",
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: Apache Software License",
+    ]    
 )
