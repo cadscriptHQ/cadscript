5c114429c45a6a4fd7e6c1a85ebd155eb7eedc26
Andreas Kahler <mail@andreaskahler.com>
2023-12-30 02:28:54 +0100

    bump version to 0.2.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='cadscript',
     description = "A Python module for creating 3D models with scripts.",
     long_description="A Python module for creating 3D models with scripts.",
-    version='0.2.1',
+    version='0.2.2',
     url="https://github.com/CadQuery/cadquery",
     license="Apache Public License 2.0",    
     author='Andreas Kahler',
