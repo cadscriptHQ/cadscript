0e7d3a6f8813c37a9f1012dc9e834737fae54439
Andreas Kahler <mail@andreaskahler.com>
2023-12-31 11:15:39 +0100

    fix example

--- examples/getting_started.py
+++ examples/getting_started.py
@@ -5,8 +5,8 @@ result.fillet("|Z", 3)
 result.chamfer("#Z", 0.6)
 
 sketch = cadscript.make_sketch()
-sketch.add_rect(10, 10)
-sketch.add_circle(diameter=10, positions=[(5, 0), (0, 5)])
+sketch.add_rect(8, 8)
+sketch.add_circle(diameter=8, positions=[(4, 0), (0, 4)])
 
 result.cut_extrude(">Z", sketch.rotate(45), -4)
 
