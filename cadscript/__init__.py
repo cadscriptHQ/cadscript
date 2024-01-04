44b221e8d82574eb321c356e90cd5df167af2d0c
Andreas Kahler <mail@andreaskahler.com>
2024-01-04 18:03:08 +0100

    improving documentation

--- cadscript/__init__.py
+++ cadscript/__init__.py
@@ -19,6 +19,20 @@ from .patterns import *
 
 
 def make_box(sizex: DimensionDefinitionType, sizey: DimensionDefinitionType, sizez: DimensionDefinitionType, center: CenterDefinitionType=True) -> 'Body':
+    """
+    Create a box-shaped body with the given dimensions.
+
+    Args:
+        sizex (DimensionDefinitionType): The size of the box along the x-axis.
+        sizey (DimensionDefinitionType): The size of the box along the y-axis.
+        sizez (DimensionDefinitionType): The size of the box along the z-axis.
+        center (CenterDefinitionType, optional): Whether to center the box at the origin. If False, the box will start from the origin.
+            Can also be "X", "Y" or "Z" to center in only one direction or "XY", "XZ", "YZ" to center in two directions.
+            Defaults to True which centers the box in all directions.
+
+    Returns:
+        Body: The created box-shaped body.
+    """
     dimx,dimy,dimz = get_dimensions([sizex, sizey, sizez], center)
     return __make_box_min_max(dimx[0],dimx[1],dimy[0],dimy[1],dimz[0],dimz[1])
 
@@ -28,6 +42,18 @@ def __make_box_min_max(x1: float, x2: float, y1: float, y2: float, z1: float, z2
     return Body(wp)
 
 def make_extrude(sketch: Sketch, amount: float, plane: Optional[Union[ConstructionPlane,str]]=None):
+    """
+    Create an extrusion from a sketch.
+
+    Args:
+        sketch (Sketch): The sketch to extrude.
+        amount (float): The amount of extrusion.
+        plane (Optional[Union[ConstructionPlane,str]], optional): The plane to extrude on. Defaults to the XY plane.
+            Can be one of the strings "XY", "YZ", "XZ", "front", "back", "top", "bottom", "left" or "right"
+
+    Returns:
+        Body: The extruded body.
+    """
     if plane is None:
         wp = cq.Workplane()
     elif isinstance(plane, str):
@@ -43,6 +69,18 @@ def make_text(
     height: float,
     font: str = "Arial",
 ):
+    """
+    Create a 3D text object.
+
+    Args:
+        text (str): The text to be created.
+        size (float): The size of the text.
+        height (float): The height of the text.
+        font (str, optional): The font of the text. Defaults to "Arial".
+
+    Returns:
+        Body: The 3D text object.
+    """
     c = cq.Compound.makeText(text, size, height, font=font)
     wp = cq.Workplane(obj = c)
     return Body(wp)
@@ -54,18 +92,36 @@ def make_construction_plane(planeStr: str, offset:Optional[float]=None):
     return ConstructionPlane(wp)
 
 def make_sketch():
+    """
+    Creates a new empty sketch object.
+
+    Returns:
+        Sketch: The newly created sketch object.
+    """
     sketch = cq.Sketch()
     return Sketch(sketch)
 
 def import_step(path):
+    """
+    Import a STEP file and return a Body object.
+
+    Args:
+        path (str): The path to the STEP file.
+
+    Returns:
+        Body: The imported body as a Body object.
+    """
     wp = cq.importers.importStep(path)
     return Body(wp)
 
-'''
-If inside CQ-Editor, use the show_object function to show the object.
-Otherwise, do nothing
-'''
 def show(item: Union[Body,Sketch,ConstructionPlane,Assembly]):
+    '''
+    If inside CQ-Editor, will display the item in the 3D view.
+    Otherwise will do nothing.
+
+    Args:
+        item: the item to show. Can be a `Body` or a `Sketch`
+    '''
     show_fn = __get_show_fn()
     if show_fn:
         show_fn(item.cq())
