9696d886f2049d1d79c788b52391d24849481b97
Andreas Kahler <mail@andreaskahler.com>
2024-01-09 18:56:54 +0100

    bracket example

---
+++ examples/bracket.py
@@ -0,0 +1,29 @@
+# Copyright (C) 2023-2024 Andreas Kahler
+# This file is part of Cadscript
+# SPDX-License-Identifier: Apache-2.0
+
+import cadscript as cad
+
+sketch = cad.make_sketch()
+sketch.add_rect(70, 5, center=False)
+sketch.add_circle(d=26, positions=[(0,10)])
+sketch.fillet("ALL", 2)
+sketch.cut_circle(d=12, positions=[(0,10)])
+sketch.cut_rect(100, (-100,0), center="X")
+
+sketch2 = cad.make_sketch()
+sketch2.add_rect(60, 30)
+sketch2.add_rect(50, 20, center="Y")
+sketch2.add_circle(d=30, positions=[(50,0)])
+sketch2.fillet("ALL", 2)
+sketch2.cut_circle(d=16, positions=[(50,0)])
+sketch2.cut_rect((-50,20), 16, center="Y")
+
+extr1 = cad.make_extrude("XZ", sketch, (-100, 100))
+extr2 = cad.make_extrude("XY", sketch2, (-100, 100))
+
+result = extr1.intersect(extr2)
+
+cad.show(result) 
+
+
