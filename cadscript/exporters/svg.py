50f715038cd875cb200b38bc495c2404b467c9ae
Andreas Kahler <mail@andreaskahler.com>
2024-01-14 14:09:23 +0100

    added svg example code for debugging

--- cadscript/exporters/svg.py
+++ cadscript/exporters/svg.py
@@ -272,12 +272,57 @@ def get_svg(shape, opts=None):
     return svg
 
 
-def render_svg(shape, fileName: str, opts=None):
-    """
-    Accept a cadquery shape, and export it to the provided file
-    """
+if __name__ == "__main__":    
 
-    svg = get_svg(shape, opts)
-    f = open(fileName, "w")
-    f.write(svg)
-    f.close()
+    import cadscript as cad
+
+    s = cad.make_sketch()
+    s.add_rect(40, 20)
+    s.cut_circle(d=6)
+
+    plate1 = cad.make_extrude("XY", s, -4)
+    plate2 = cad.make_extrude("XY", s.rotate(90), (-8, -4))
+
+    peg = cad.make_extrude("XY", cad.make_sketch().add_circle(d=6), -20)
+    peg = peg.add(cad.make_extrude("XY", cad.make_sketch().add_circle(d=10), 5))
+
+    svg_options = {
+        "projectionDir": (-1.75, 4, 1),
+        "showHidden": True,
+        "width": 600,
+        "height": 400,
+    }
+
+    style1 = {
+        "visible": {
+            "stroke": "rgb(90,90,90)",
+            "stroke-width": ".2",
+        },
+        "hidden": {
+            "stroke": "rgb(160,160,160)",
+            "stroke-width": "0.1",
+            "stroke-dasharray": "0.15,0.15",
+        },
+    }
+    style2 = {
+        "visible": {
+            "stroke": "rgb(200,0,0)",
+            "stroke-width": ".3",
+        },
+        "hidden": {
+            "stroke": "rgb(255,160,160)",
+            "stroke-width": "0.1",
+            "stroke-dasharray": "0.15,0.15",
+        },
+    }
+
+    shapes = [
+        (plate1.cq().findSolid(), style1),
+        (plate2.cq().findSolid(), style1),
+        (peg.cq().findSolid(), style2),
+    ]
+    out_svg = get_svg(shapes, svg_options)
+
+    with open("private.svg", "w") as f:
+        f.write(out_svg)
+
