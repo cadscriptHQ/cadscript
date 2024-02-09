04881bd24c015e95edad899c9501fc99bd5d4a43
Andreas Kahler <mail@andreaskahler.com>
2024-02-10 00:31:28 +0100

    doc typo

--- cadscript/__init__.py
+++ cadscript/__init__.py
@@ -109,7 +109,7 @@ def make_cylinder(*,
         radius (float, optional): The radius of the cylinder (alternative to 'r', 'd' or 'diameter').
         d (float, optional): The diameter of the cylinder (alternative to 'r', 'radius' or 'diameter').
         diameter (float, optional): The diameter of the cylinder (alternative to 'r', 'radius' or 'd').
-        center (CenterDefinitionType, optional): Whether to center the box at the cylinder. If False, the box will start from the origin.
+        center (CenterDefinitionType, optional): Whether to center the cylinder at the origin. If False, the box will start from the origin.
             Can also be "X", "Y" or "Z" to center in only one direction or "XY", "XZ", "YZ" to center in two directions.
             if "base" is specified, the cylinder will be centered at the base, e.g. in XY if direction is "Z".
             Defaults to True which centers the box in all directions.
