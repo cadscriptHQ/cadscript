b8fd26e0f0681d9f6ae02ff60b8b2df92edd32b2
Andreas Kahler <mail@andreaskahler.com>
2024-02-15 11:59:06 +0100

    interval classes: added set, expand and shrink methods

--- cadscript/interval.py
+++ cadscript/interval.py
@@ -1,6 +1,6 @@
-
-
-
+# Copyright (C) 2023-2024 Andreas Kahler
+# This file is part of Cadscript
+# SPDX-License-Identifier: Apache-2.0
 
 from typing import Tuple
 from .typedefs import Vector2DType, Vector3DType
@@ -10,27 +10,33 @@ class Interval1D:
     """
     A class to represent a 1D interval.
     """
-
     x1: float
     x2: float
 
     def __init__(self, x1: float, x2: float):
+        self.set(x1, x2)
+
+    def set(self, x1: float, x2: float) -> 'Interval1D':
         if x1 > x2:
             raise ValueError("x1 must be less than or equal to x2")
         self.x1 = x1
         self.x2 = x2
+        return self
 
     @staticmethod
-    def from_tuple(extent: Tuple[float, float]) -> "Interval1D":
+    def from_tuple(extent: Tuple[float, float]) -> 'Interval1D':
         return Interval1D(extent[0], extent[1])
 
     def __repr__(self):
-        return f"({self.x1}, {self.x2})"
+        return f"Interval1D({self.x1}, {self.x2})"
 
     def __eq__(self, __value: object) -> bool:
         if not isinstance(__value, Interval1D):
             return False
         return self.x1 == __value.x1 and self.x2 == __value.x2
+
+    def copy(self) -> 'Interval1D':
+        return Interval1D(self.x1, self.x2)
 
     @property
     def min(self) -> float:
@@ -49,6 +55,14 @@ class Interval1D:
     def center(self) -> float:
         return (self.x1 + self.x2) / 2
 
+    def expand(self, offset: float) -> 'Interval1D':
+        return self.set(self.x1 - offset, self.x2 + offset)
+
+    def shrink(self, offset: float, raise_if_zero: bool = True) -> 'Interval1D':
+        if raise_if_zero and (self.x2 - self.x1) < 2 * offset:
+            raise ValueError("Interval is too small to shrink")
+        return self.set(self.x1 + offset, self.x2 - offset)
+
 
 class Interval2D:
     """
@@ -61,6 +75,9 @@ class Interval2D:
     y2: float
 
     def __init__(self, x1: float, x2: float, y1: float, y2: float):
+        self.set(x1, x2, y1, y2)
+
+    def set(self, x1: float, x2: float, y1: float, y2: float) -> 'Interval2D':
         if x1 > x2:
             raise ValueError("x1 must be less than or equal to x2")
         if y1 > y2:
@@ -69,18 +86,22 @@ class Interval2D:
         self.x2 = x2
         self.y1 = y1
         self.y2 = y2
+        return self
 
     @staticmethod
-    def from_tuples(x: Tuple[float, float], y: Tuple[float, float]) -> "Interval2D":
+    def from_tuples(x: Tuple[float, float], y: Tuple[float, float]) -> 'Interval2D':
         return Interval2D(x[0], x[1], y[0], y[1])
 
     def __repr__(self):
-        return f"({self.x1}, {self.x2}), ({self.y1}, {self.y2})"
+        return f"Interval2D({self.x1}, {self.x2}), ({self.y1}, {self.y2})"
 
     def __eq__(self, __value: object) -> bool:
         if not isinstance(__value, Interval2D):
             return False
         return self.x1 == __value.x1 and self.x2 == __value.x2 and self.y1 == __value.y1 and self.y2 == __value.y2
+
+    def copy(self) -> 'Interval2D':
+        return Interval2D(self.x1, self.x2, self.y1, self.y2)
 
     @property
     def min_x(self) -> float:
@@ -134,6 +155,27 @@ class Interval2D:
     def center_y(self) -> float:
         return (self.y1 + self.y2) / 2
 
+    def expand_x(self, offset: float) -> 'Interval2D':
+        return self.set(self.x1 - offset, self.x2 + offset, self.y1, self.y2)
+
+    def expand_y(self, offset: float) -> 'Interval2D':
+        return self.set(self.x1, self.x2, self.y1 - offset, self.y2 + offset)
+
+    def expand(self, offset: float) -> 'Interval2D':
+        return self.set(self.x1 - offset, self.x2 + offset, self.y1 - offset, self.y2 + offset)
+
+    def shrink_x(self, offset: float, raise_if_zero: bool = True) -> 'Interval2D':
+        if raise_if_zero and (self.x2 - self.x1) < 2 * offset:
+            raise ValueError("Interval is too small to shrink")
+        return self.set(self.x1 + offset, self.x2 - offset, self.y1, self.y2)
+
+    def shrink_y(self, offset: float, raise_if_zero: bool = True) -> 'Interval2D':
+        if raise_if_zero and (self.y2 - self.y1) < 2 * offset:
+            raise ValueError("Interval is too small to shrink")
+        return self.set(self.x1, self.x2, self.y1 + offset, self.y2 - offset)
+
+    def shrink(self, offset: float, raise_if_zero: bool = True) -> 'Interval2D':
+        return self.shrink_x(offset, raise_if_zero).shrink_y(offset, raise_if_zero)
 
 
 class Interval3D:
@@ -148,6 +190,9 @@ class Interval3D:
     z2: float
 
     def __init__(self, x1: float, x2: float, y1: float, y2: float, z1: float, z2: float):
+        self.set(x1, x2, y1, y2, z1, z2)
+
+    def set(self, x1: float, x2: float, y1: float, y2: float, z1: float, z2: float) -> 'Interval3D':
         if x1 > x2:
             raise ValueError("x1 must be less than or equal to x2")
         if y1 > y2:
@@ -160,19 +205,23 @@ class Interval3D:
         self.y2 = y2
         self.z1 = z1
         self.z2 = z2
+        return self
 
     @staticmethod
-    def from_tuples(x: Tuple[float, float], y: Tuple[float, float], z: Tuple[float, float]) -> "Interval3D":
+    def from_tuples(x: Tuple[float, float], y: Tuple[float, float], z: Tuple[float, float]) -> 'Interval3D':
         return Interval3D(x[0], x[1], y[0], y[1], z[0], z[1])
 
     def __repr__(self):
-        return f"({self.x1}, {self.x2}), ({self.y1}, {self.y2}), ({self.z1}, {self.z2})"
+        return f"Interval3D({self.x1}, {self.x2}), ({self.y1}, {self.y2}), ({self.z1}, {self.z2})"
 
     def __eq__(self, __value: object) -> bool:
         if not isinstance(__value, Interval3D):
             return False
         return (self.x1 == __value.x1 and self.x2 == __value.x2 and self.y1 == __value.y1 and
                 self.y2 == __value.y2 and self.z1 == __value.z1 and self.z2 == __value.z2)
+
+    def copy(self) -> 'Interval3D':
+        return Interval3D(self.x1, self.x2, self.y1, self.y2, self.z1, self.z2)
 
     @property
     def min_x(self) -> float:
@@ -245,3 +294,33 @@ class Interval3D:
 
     def center_z(self) -> float:
         return (self.z1 + self.z2) / 2
+
+    def expand_x(self, offset: float) -> 'Interval3D':
+        return self.set(self.x1 - offset, self.x2 + offset, self.y1, self.y2, self.z1, self.z2)
+
+    def expand_y(self, offset: float) -> 'Interval3D':
+        return self.set(self.x1, self.x2, self.y1 - offset, self.y2 + offset, self.z1, self.z2)
+
+    def expand_z(self, offset: float) -> 'Interval3D':
+        return self.set(self.x1, self.x2, self.y1, self.y2, self.z1 - offset, self.z2 + offset)
+
+    def expand(self, offset: float) -> 'Interval3D':
+        return self.set(self.x1 - offset, self.x2 + offset, self.y1 - offset, self.y2 + offset, self.z1 - offset, self.z2 + offset)
+
+    def shrink_x(self, offset: float, raise_if_zero: bool = True) -> 'Interval3D':
+        if raise_if_zero and (self.x2 - self.x1) < 2 * offset:
+            raise ValueError("Interval is too small to shrink")
+        return self.set(self.x1 + offset, self.x2 - offset, self.y1, self.y2, self.z1, self.z2)
+
+    def shrink_y(self, offset: float, raise_if_zero: bool = True) -> 'Interval3D':
+        if raise_if_zero and (self.y2 - self.y1) < 2 * offset:
+            raise ValueError("Interval is too small to shrink")
+        return self.set(self.x1, self.x2, self.y1 + offset, self.y2 - offset, self.z1, self.z2)
+
+    def shrink_z(self, offset: float, raise_if_zero: bool = True) -> 'Interval3D':
+        if raise_if_zero and (self.z2 - self.z1) < 2 * offset:
+            raise ValueError("Interval is too small to shrink")
+        return self.set(self.x1, self.x2, self.y1, self.y2, self.z1 + offset, self.z2 - offset)
+
+    def shrink(self, offset: float, raise_if_zero: bool = True) -> 'Interval3D':
+        return self.shrink_x(offset, raise_if_zero).shrink_y(offset, raise_if_zero).shrink_z(offset, raise_if_zero)
